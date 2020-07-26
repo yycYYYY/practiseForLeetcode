a8190f16d24b766a5be4f6246d42b04a0529827e
matthewot <616539757@qq.com>
2020-07-26 18:05:30 +0800

    NO.24

--- src/main/java/com/practice/P1_100/P21_30/swapPairs.java
+++ src/main/java/com/practice/P1_100/P21_30/swapPairs.java
@@ -3,23 +3,35 @@ package com.practice.P1_100.P21_30;
 import com.practice.dataStructure.ListNode;
 
 public class swapPairs {
-//    如同合并两个有序链表一样，本题也有递归，迭代两个解法，递归需要占用更多的栈空间
-//    空间复杂度更高
+    /*
+    NO.24
+
+        如同合并两个有序链表一样，本题也有递归，迭代两个解法，递归需要占用更多的栈空间
+    空间复杂度更高
+     */
+
 
     ListNode solution(ListNode ln){
 
+        if (ln == null || ln.next == null) return ln;
+
         ListNode res = new ListNode(-1);
-        res.next = ln;
-        ListNode cur = res;
+        ListNode pre = new ListNode();
+        
+        res.next = ln.next;
+        while (ln != null&&ln.next != null){
+            ListNode firstNode = ln;
+            ListNode secondNode = ln.next;
 
-        while (ln.next != null&&ln.next.next != null){
-            ListNode firstNode = ln.next;
-            ListNode secondNode = ln.next.next;
-
+            firstNode.next = secondNode.next;
             secondNode.next = firstNode;
-            cur.next = firstNode;
+            ln = secondNode;
+
+            pre.next = ln;
+            ln = ln.next.next;
+            pre = firstNode;
 
         }
-        return null;
+        return res.next;
     }
 }
---
+++ src/main/java/com/practice/P1_100/P81_90/deleteDuplicates.java
@@ -0,0 +1,23 @@
+package com.practice.P1_100.P81_90;
+
+import com.practice.dataStructure.ListNode;
+
+public class deleteDuplicates {
+    /*
+    NO.83
+    删除链表中重复的元素
+    给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+     */
+
+    /**
+     *  本题的思路比较简单，直接：
+     *  对链表进行循环遍历，将每一位元素存到哈希表中，以LN的value当做Map中的key
+     *  对每一位的元素进行遍历的同时，查询map中是否存在这个值，如果存在则删除这个与元素
+     */
+    ListNode solution(ListNode head){
+        ListNode res = head;
+
+
+        return res;
+    }
+}
