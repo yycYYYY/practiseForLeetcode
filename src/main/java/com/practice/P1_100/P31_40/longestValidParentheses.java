b2d439590103cdde0c5170d697383fbd20bf6082
yuyongchao <yuyongchao@itiger.com>
2020-08-14 19:13:23 +0800

    NO.70 complete

---
+++ src/main/java/com/practice/P1_100/P31_40/longestValidParentheses.java
@@ -0,0 +1,21 @@
+package com.practice.P1_100.P31_40;
+
+public class longestValidParentheses {
+    /*
+    NO.32 最长有效括号
+    给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+
+    示例 1:
+    输入: "(()"
+    输出: 2
+    解释: 最长有效括号子串为 "()"
+
+    示例 2:
+    输入: ")()())"
+    输出: 4
+    解释: 最长有效括号子串为 "()()"
+     */
+    String solution1(String s){
+        return "";
+    }
+}
