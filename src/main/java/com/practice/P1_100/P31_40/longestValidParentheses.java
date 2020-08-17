d919d91e120ceabb6c714cb31ae196185965f281
yuyongchao <yuyongchao@itiger.com>
2020-08-17 20:44:27 +0800

    NO.32 thinking

--- src/main/java/com/practice/P1_100/P31_40/longestValidParentheses.java
+++ src/main/java/com/practice/P1_100/P31_40/longestValidParentheses.java
@@ -15,7 +15,34 @@ public class longestValidParentheses {
     输出: 4
     解释: 最长有效括号子串为 "()()"
      */
-    String solution1(String s){
-        return "";
+    int solution1(String s){
+        int max = 0;
+        int len = s.length();
+        if (len < 2) return 0;
+        int[] dp = new int[len];
+        //dp的定义也有问题，dp[i]为以i结尾的子串长度，若i为（则dp为0
+        dp[0] = 0;
+        for (int i = 1;i < len;i++){
+            if (s.charAt(i) == ')'){
+                /*
+                大体思路是对的，但是没有考虑到()(())、()(())、()(())((((()
+                这三种case的情况，需要对dp[i]的状态转移方程做一个修正
+                状态转移的条件对了，但是状态转移方程（值的变化方式）错了
+                 */
+                if (s.charAt(i-1) == '('){
+                    dp[i] = (i >= 2?dp[i-2]:0) + 2;
+                }else if (i - dp[i-1] -1 >= 0 && dp[i - dp[i-1] -1] == '('){
+                    dp[i] = dp[i-1] + 2;
+                }
+            }
+            max = Math.max(max,dp[i]);
+        }
+        return dp[len-1];
+    }
+
+    public static void main(String[] args) {
+        longestValidParentheses l = new longestValidParentheses();
+        int a = l.solution1("()(())())))))(");
+        System.out.println(a);
     }
 }
