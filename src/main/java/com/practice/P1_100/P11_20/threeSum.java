bba87803c01e8941d9b860fb37ab408d3638bfa5
yuyongchao <yuyongchao@itiger.com>
2020-08-25 20:20:51 +0800

    NO.15 thinking

---
+++ src/main/java/com/practice/P1_100/P11_20/threeSum.java
@@ -0,0 +1,30 @@
+package com.practice.P1_100.P11_20;
+
+import com.practice.dataStructure.ListNode;
+
+import java.util.List;
+
+public class threeSum {
+    /*
+    NO.15 三数之和
+    给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+
+    注意：答案中不可以包含重复的三元组。
+
+    示例：
+    给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+    满足要求的三元组集合为：
+    [
+      [-1, 0, 1],
+     [-1, -1, 2]
+    ]
+     */
+
+    /*
+    先用最笨的方法，穷举获得所有的符合条件三元组，然后再去重
+     */
+    List<List<Integer>> solution(int[] nums){
+        if (nums == null || nums.length <3) return null;
+        return null;
+    }
+}
