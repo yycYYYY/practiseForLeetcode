1ef8f6151bd7f8c96cd2d05d92d9c87de69fcd05
yuyongchao <yuyongchao@itiger.com>
2020-08-27 11:30:24 +0800

    NO.15 complete

--- src/main/java/com/practice/P1_100/P11_20/threeSum.java
+++ src/main/java/com/practice/P1_100/P11_20/threeSum.java
@@ -2,7 +2,7 @@ package com.practice.P1_100.P11_20;
 
 import com.practice.dataStructure.ListNode;
 
-import java.util.List;
+import java.util.*;
 
 public class threeSum {
     /*
@@ -21,10 +21,71 @@ public class threeSum {
      */
 
     /*
-    先用最笨的方法，穷举获得所有的符合条件三元组，然后再去重
+    先数组排序，然后用twosum的方式遍历，这种方式是错误的。复杂度很高，还需要考虑后续的去重，非常恶心
      */
     List<List<Integer>> solution(int[] nums){
         if (nums == null || nums.length <3) return null;
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        int temp = 0;
+        HashSet<Integer> tv = new HashSet<>();
+        tv.add(nums[0]);
+
+        for (int l = 1;l < nums.length;l++){
+            if (!tv.contains(nums[l])) tv.add(nums[l]);
+        }
+
+        Arrays.sort(nums);
+        for(int i = 0;i< nums.length-2;i++){
+            if (i == 0 || nums[i] != nums[i - 1]){
+
+                for (int j = i+1;j < nums.length - 1;j++){
+                    if (j == 0 || nums[j] != nums[j - 1]){
+                        temp = -nums[i] - nums[j];
+                        if (tv.contains(temp)){
+                            List<Integer> lt = new ArrayList<>();
+                            lt.add(nums[i]);
+                            lt.add(nums[j]);
+                            lt.add(temp);
+                            res.add(lt);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    //用评论里的排序双指针，不再使用哈希表匹配，哈希表匹配后续的去重比较恶心
+    List<List<Integer>> solution2(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length <3) return res;
+        Arrays.sort(nums);
+        for (int i = 0;i < nums.length && nums[i] <= 0;i++){
+            if (i ==0 || nums[i] != nums[i-1]){
+                int L = i + 1,R = nums.length-1;
+                while (L<R){
+                    int sum = nums[i]+nums[L]+nums[R];
+                    if (sum == 0){
+                        res.add(Arrays.asList(nums[i],nums[L],nums[R]));
+
+                        while(L<R&&nums[L] == nums[L+1]) L++;
+                        while(L<R&&nums[R] == nums[R-1]) R--;
+
+                        L++;
+                        R--;
+                    }else if (sum < 0) L++;
+                    else R--;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        threeSum t = new threeSum();
+        int[] a = new int[]{-1,-1,1,0};
+        List b = t.solution2(a);
+        System.out.println(b);
     }
 }
