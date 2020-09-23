a4cdcb23234531694401118040619cb0075aaeea
yuyongchao <yuyongchao@itiger.com>
2020-09-23 20:37:23 +0800

    add NO.34

---
+++ src/main/java/com/practice/P1_100/P31_40/SearchRange.java
@@ -0,0 +1,24 @@
+package com.practice.P1_100.P31_40;
+
+public class SearchRange {
+    /*
+    NO.34 在排序数组中查找元素的第一个和最后一个位置
+    给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+    你的算法时间复杂度必须是 O(log n) 级别。
+    如果数组中不存在目标值，返回 [-1, -1]。
+
+    示例 1:
+    输入: nums = [5,7,7,8,8,10], target = 8
+    输出: [3,4]
+
+    示例 2:
+    输入: nums = [5,7,7,8,8,10], target = 6
+    输出: [-1,-1]
+     */
+    int[] solution(int[] nums,int target){
+        int[] res = new int[]{-1,-1};
+        int left = 0,right = nums.length - 1,mid = left + nums.length/2;
+
+        return res;
+    }
+}
